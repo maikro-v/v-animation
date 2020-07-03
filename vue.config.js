38016a4c24360ea1dd168e15611d3be302ac0b1e
hsgzhuanyong <hsgzhuanyong@126.com>
2020-07-03 10:14:37 +0800

    docs: 修改文档

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,12 @@
 const nodeEnv = process.env.NODE_ENV
 let outputDir = 'dist'
-let publicPath = '/'
 if (nodeEnv === 'examples') {
   outputDir = 'exampleDocs'
-  publicPath = '/v-animation/exampleDocs'
 } else if (nodeEnv === 'ui') {
   outputDir = 'main'
 }
 
 module.exports = {
-  publicPath,
   pages: {
     index: {
       entry: './examples/index.js',
